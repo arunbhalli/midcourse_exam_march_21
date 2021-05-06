f99ffb06545ae7d4b26f8def868da15bd53f05b6
arunbhalli <tomarbhalli@gmail.com>
2021-05-06 12:06:53 +0200

    add methode for username

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -3,13 +3,28 @@ import { Component } from 'react';
 import { Button, Input } from 'semantic-ui-react';
 
 class GHSearch extends Component {
-  state = {
+  
+  constructor (props){
+    
+    this.state = {
+      username:'',
+    };
+  }
+  getUser(username){
+    return fetch(`https://api.github.com/users/${username}`)
+    .then(response => response.json())
+    .then(response => {
+      return response
+    })
+  }
+  async handleSearch(){
+    this.setState({username:user.login})
   }
 	render() {
 		return (
 			<>
 				<Input type="text" name="search" placeholder="Input GH username" />
-				<Button name="search">Search</Button>
+				<Button name="search" onClick={() => {this.handleSearch()}}>Search </Button>
 			</>
 		);
 	}
