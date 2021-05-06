44d74886a58a7b22a4ac2a21b81617ee77087ca5
arunbhalli <tomarbhalli@gmail.com>
2021-05-06 11:34:22 +0200

    Add test for user search function

--- cypress/integration/userCanSeeApplicationTitle.feature.js
+++ cypress/integration/userCanSeeApplicationTitle.feature.js
@@ -1,10 +1,16 @@
 describe('Application main view', () => {
   beforeEach(() => {
-    cy.visit('/')
+   cy.visit("/")
   });
 
   it('contains titel', () => {
     cy.get("section[name='title']")
       .should('contain', 'GitHub Search engine')
   });
+  it("display the expected dtails of user", () => {
+    cy.get("[data-cy=search]").within(() => {
+      cy.get("[data-cy=user-name]").should("contain","Barack")
+    })
+  })
+
 });
